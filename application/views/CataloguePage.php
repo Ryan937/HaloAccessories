10503892b5dec40d6660026282009442f2e1f0e5
Piyotr-K <piyotrkao1998@Outlook.com>
2018-02-08 22:00:15 -0800

    Added a cataloguepage and controller
    Signed-off-by: Piyotr-K <piyotrkao1998@Outlook.com>

---
+++ application/views/CataloguePage.php
@@ -0,0 +1,11 @@
+<table>
+    <tr>
+        <td>
+            <h2>Helmets :</h2>
+        </td>
+        <td>
+            <img src="/assets/images/Helmet_Tier1.PNG"/>
+            <p style="text-align: center;">Tier 1</p>
+        </td>
+    </tr>
+</table>
