b23e63bd9c2e3ec2f999ef2f6d2ff2fc8d2246a8
Piyotr-K <piyotrkao1998@Outlook.com>
2018-02-08 22:05:04 -0800

    Finished catalogue page
    Signed-off-by: Piyotr-K <piyotrkao1998@Outlook.com>

--- application/views/CataloguePage.php
+++ application/views/CataloguePage.php
@@ -1,11 +1,86 @@
-<table>
+<table style="padding: 20px;">
     <tr>
         <td>
             <h2>Helmets :</h2>
         </td>
         <td>
-            <img src="/assets/images/Helmet_Tier1.PNG"/>
+            <img src="/assets/images/Helmet_Tier1.PNG" style="padding: 30px;"/>
             <p style="text-align: center;">Tier 1</p>
+        </td>
+        <td>
+            <img src="/assets/images/Helmet_Tier2.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 2</p>
+        </td>
+        <td>
+            <img src="/assets/images/Helmet_Tier3.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 3</p>
+        </td>
+        <td>
+            <img src="/assets/images/Helmet_Tier4.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 4</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h2>Shoulder Pads :</h2>
+        </td>
+        <td>
+            <img src="/assets/images/Shoulder_Tier1.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 1</p>
+        </td>
+        <td>
+            <img src="/assets/images/Shoulder_Tier2.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 2</p>
+        </td>
+        <td>
+            <img src="/assets/images/Shoulder_Tier3.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 3</p>
+        </td>
+        <td>
+            <img src="/assets/images/Shoulder_Tier4.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 4</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h2>Chest :</h2>
+        </td>
+        <td>
+            <img src="/assets/images/Chest_Tier1.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 1</p>
+        </td>
+        <td>
+            <img src="/assets/images/Chest_Tier2.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 2</p>
+        </td>
+        <td>
+            <img src="/assets/images/Chest_Tier3.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 3</p>
+        </td>
+        <td>
+            <img src="/assets/images/Chest_Tier4.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 4</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <h2>Wrist Guard :</h2>
+        </td>
+        <td>
+            <img src="/assets/images/Wrist_Tier1.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 1</p>
+        </td>
+        <td>
+            <img src="/assets/images/Wrist_Tier2.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 2</p>
+        </td>
+        <td>
+            <img src="/assets/images/Wrist_Tier3.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 3</p>
+        </td>
+        <td>
+            <img src="/assets/images/Wrist_Tier4.PNG" style="padding: 30px;"/>
+            <p style="text-align: center;">Tier 4</p>
         </td>
     </tr>
 </table>
